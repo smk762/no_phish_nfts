1e4958ec78ad74eb85c01092b95da02e0d321e5b
smk762 <smk762@iinet.net.au>
2023-08-18 15:20:47 +0000

    add logo to docs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, status
+from fastapi.openapi.utils import get_openapi
 
 from core.config import settings
 from api.router import router
@@ -17,6 +18,27 @@ app = FastAPI(
 )
 
 app.include_router(router, prefix=settings.api_prefix)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=settings.title,
+        version=settings.version,
+        description=settings.description,
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://komodoplatform.com/assets/img/logo-dark.webp",
+        "backgroundColor": "#CCCCCC",
+        "altText": "Komodoplatform logo"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 
 @app.get("/")
