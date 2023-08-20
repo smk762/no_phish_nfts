748523644b4e6c7243a1b7fa4079a9cfdfc46aa1
smk762 <smk762@iinet.net.au>
2023-08-20 09:32:18 +0000

    add api_key auth

---
+++ auth.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from fastapi import Depends, HTTPException, status
+from fastapi.security.api_key import APIKeyHeader
+from fastapi import Security, HTTPException
+from core.config import settings
+
+api_key_header = APIKeyHeader(name="access_token", auto_error=False)
+
+
+class ApiKeyEnum(str, Enum):
+    edit = "edit"
+    view = "view"
+    
+    def __str__(self) -> str:
+        return str.__str__(self)
+
+
+async def edit_api_key_auth(api_key: str = Security(api_key_header)):
+    if api_key not in settings.valid_api_keys[ApiKeyEnum.edit]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Your API key does not have access to this endpoint"
+        )
+
+# TODO: Not currently in use, but may be activated if required
+async def view_api_key_auth(api_key: str = Security(api_key_header)):
+    if api_key not in settings.valid_api_keys[ApiKeyEnum.view]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Your API key does not have access to this endpoint"
+        )
