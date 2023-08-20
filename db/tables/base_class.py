0bc8588a897d2231879c8acdda79020a0db147e7
smk762 <smk762@iinet.net.au>
2023-08-20 05:55:57 +0000

    lowercase for network enum

--- db/tables/base_class.py
+++ db/tables/base_class.py
@@ -1,25 +1,22 @@
 from datetime import datetime
 from uuid import UUID, uuid4
-
-from sqlalchemy import Enum, text
+from enum import Enum
+from sqlalchemy import text
 from sqlmodel import Field, SQLModel
 
 
-class StatusEnum(str, Enum):
-    active = "active"
-    inactive = "inactive"
-    deleted = "deleted"
-
-# TODO: Use this, once confirming expected netwwork value (match to moralis)
 class NetworkEnum(str, Enum):
-    avalanche = "Avalanche"
-    bsc = "Bsc"
-    eth = "Eth"
-    fantom = "Fantom"
-    polygon = "Polygon"
+    # If adding more, match to Moralis API chains
+    avalanche = "avalanche"
+    bsc = "bsc"
+    eth = "ethereum"
+    fantom = "fantom"
+    polygon = "polygon"
     
     def __str__(self) -> str:
         return str.__str__(self)
+
+
 
 
 class UUIDModel(SQLModel):
