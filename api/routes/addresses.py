bde248dd26e466806a59b9edb5824ae127fe6ee6
smk762 <smk762@iinet.net.au>
2023-08-22 08:33:03 +0000

    update path param name

--- api/routes/addresses.py
+++ api/routes/addresses.py
@@ -5,15 +5,15 @@ from enums import MnemonicHqNetworkEnum
 router = APIRouter()
 
 @router.get(
-    "/{network}/{address}",
+    "/{network}/{wallet_address}",
     status_code=status.HTTP_200_OK,
     name="scan_for_spam",
-    tags=["Addresses"],
-    summary="Scans an address for spam on MnemonicHQ. Eth and Matic only.",
-    description="If spam is detected, it will be added to the local DB. Use '0x3eb4b12127EdC81A4d2fD49658db07005bcAd065' as an example on ETH address to return a positive result.",
+    tags=["Wallet Addresses"],
+    summary="Scans an address for spam on MnemonicHQ. Eth / Polygon only.",
+    description="If spam is detected, it will be added to the local DB. Use '0x3eb4b12127EdC81A4d2fD49658db07005bcAd065' as an example address to return a positive result (both ETH and POLYGON).",
 )
 async def scan_for_spam(
     network: MnemonicHqNetworkEnum,
-    address: str    
+    wallet_address: str    
 ):
-    return spam_scan(network, address)
+    return spam_scan(network, wallet_address)
